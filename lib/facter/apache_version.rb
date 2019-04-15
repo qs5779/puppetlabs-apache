80f5ad8a166698c7ddf9d9aa5533d3cd7a8d2419
Quien Sabe <qs5779@mail.com>
2019-04-15 16:47:36 -0400

    fedora support

--- lib/facter/apache_version.rb
+++ lib/facter/apache_version.rb
@@ -2,7 +2,11 @@ Facter.add(:apache_version) do
   confine kernel: ['FreeBSD', 'Linux']
   setcode do
     if Facter::Util::Resolution.which('apachectl')
-      apache_version = Facter::Util::Resolution.exec('apachectl -v 2>&1')
+      if Facter::Value(:operatingsystem) == 'Fedora'
+        apache_version = Facter::Util::Resolution.exec('httpd -v 2>&1')
+      else
+        apache_version = Facter::Util::Resolution.exec('apachectl -v 2>&1')
+      end
       Facter.debug "Matching apachectl '#{apache_version}'"
       %r{^Server version: Apache/(\d+\.\d+(\.\d+)?)}.match(apache_version)[1]
     elsif Facter::Util::Resolution.which('apache2ctl')
